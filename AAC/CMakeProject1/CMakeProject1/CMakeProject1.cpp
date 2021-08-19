4023723a60d1ede710bd7a81766ec09e1e5dad9e
fushengzhijushi <30429743+fushengzhijushi@users.noreply.github.com>
2021-08-19 11:49:19 +0800

    Add files via upload

---
+++ AAC/CMakeProject1/CMakeProject1/CMakeProject1.cpp
@@ -0,0 +1,22 @@
+﻿// CMakeProject1.cpp: 定义应用程序的入口点。
+//
+
+#include "CMakeProject1.h"
+
+using namespace std;
+
+#include <stdio.h>
+
+int simplest_aac_parser(char* url);
+
+int main(int argc, const char* argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+
+    //资源文件路径，此地址要更换为自己的电脑路径
+    char* inUrl = "C:/Users/Admin/Desktop/VideoAudioDataProcessing/AAC/resources/nocturne.aac";
+
+    simplest_aac_parser(inUrl);
+
+    return 0;
+}
---
+++ FLV/CMakeProject1/CMakeProject1/CMakeProject1.cpp
@@ -0,0 +1,28 @@
+﻿// CMakeProject1.cpp: 定义应用程序的入口点。
+//
+
+#include "CMakeProject1.h"
+
+using namespace std;
+
+#include <stdio.h>
+
+
+/**
+ * Analysis FLV file
+ * @param url    Location of input FLV file.
+ */
+
+int simplest_flv_parser(char* url);
+
+int main(int argc, const char* argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+
+    //资源文件路径，此地址要更换为自己的电脑路径
+    char* inUrl = "C:/Users/Admin/Desktop/VideoAudioDataProcessing/FLV/resources/cuc_ieschool.flv";
+
+    simplest_flv_parser(inUrl);
+
+    return 0;
+}
