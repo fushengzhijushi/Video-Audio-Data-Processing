caedf53d28dadbf33b61adb150c36e61f6933ca7
fushengzhijushi <30429743+fushengzhijushi@users.noreply.github.com>
2021-08-19 11:59:41 +0800

    Add files via upload

---
+++ H264/CMakeProject1/CMakeProject1/CMakeProject1.cpp
@@ -0,0 +1,30 @@
+﻿// CMakeProject1.cpp: 定义应用程序的入口点。
+//
+
+#include "CMakeProject1.h"
+
+using namespace std;
+
+#include <stdio.h>
+
+/**
+ * Analysis H.264 Bitstream
+ * @param url    Location of input H.264 bitstream file.
+ */
+int simplest_h264_parser(char* url);
+
+
+int main(int argc, const char* argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+
+    //资源文件路径，此地址要更换为自己的电脑路径
+    char* inUrl = "C:/Users/Admin/Desktop/VideoAudioDataProcessing/H264/resources/sintel.h264";
+
+
+    //Test
+    simplest_h264_parser(inUrl);
+
+
+    return 0;
+}
---
+++ UDPRTP/CMakeProject1/CMakeProject1/CMakeProject1.cpp
@@ -0,0 +1,25 @@
+﻿// CMakeProject1.cpp: 定义应用程序的入口点。
+//
+
+#include "CMakeProject1.h"
+
+using namespace std;
+
+#include <stdio.h>
+
+int simplest_udp_parser(int port,char* outUrl);
+
+
+
+int main(int argc, const char* argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+
+    //资源文件路径，此地址要更换为自己的电脑路径
+    char* outUrl = "C:/Users/Admin/Desktop/VideoAudioDataProcessing/UDPRTP/CMakeProject1/output_dump.ts";
+
+    simplest_udp_parser(8880,outUrl);
+
+
+    return 0;
+}
